a1d567357021cb3fc417e9ab108f61f55aba9612
Gerasimov Dmitry <gerasimov-dmr@yandex.ru>
2020-07-05 15:14:43 +0300

    added photo slider

--- js/main.js
+++ js/main.js
@@ -23,3 +23,29 @@ function copyText(event) {
 		console.log('Oops, unable to copy');  
 	}  
 }
+
+
+
+//слайдер фото
+let images = document.querySelectorAll('.photo-block__img');
+let current = 0;
+console.dir(images);
+
+function slider() {
+	for (let image of images) {
+		console.dir(image);
+		image.classList.add('opacity0');
+	}
+	images[current].classList.remove('opacity0');
+}
+
+document.querySelector('.btn-slider_right').onclick = function() {
+	current = (++current) % images.length;
+	slider();
+}
+document.querySelector('.btn-slider_left').onclick = function() {
+	current = (--current) % images.length;
+	console.log(current);
+	current = current === -1 ? images.length-1 : current;
+	slider();
+}
